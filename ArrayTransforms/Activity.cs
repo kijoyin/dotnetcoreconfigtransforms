596e00a66a192881271cbef790d722cde1209cf9
Kiran Joy <kijoyin@gmail.com>
2020-08-28 15:12:21 +1000

    Add project files.

---
+++ ArrayTransforms/Activity.cs
@@ -0,0 +1,16 @@
+﻿// Activity.cs
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ArrayTransforms
+{
+    public class Activity
+    {
+        public string Name { get; set; }
+        public string MinimumDistance { get; set; }
+        public string MinimumSpeed { get; set; }
+    }
+}
---
+++ ArrayTransforms/Controllers/ActvitiesController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Threading.Tasks.Dataflow;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+
+namespace ArrayTransforms.Controllers
+{
+    
+    [ApiController]
+    public class ActvitiesController : ControllerBase
+    {
+        private readonly IEnumerable<Activity> _activities;
+        private readonly IWebHostEnvironment _hostingEnvironment;
+
+        /// <summary>
+        /// Inject the activites config item and hosting environment 
+        /// </summary>
+        /// <param name="activitiesOptions">Activites options</param>
+        /// <param name="hostingEnvironment">To get the current environment name</param>
+        public ActvitiesController(IOptions<List<Activity>> activitiesOptions, IWebHostEnvironment hostingEnvironment)
+        {
+            _activities = activitiesOptions.Value;
+            _hostingEnvironment = hostingEnvironment;
+        }
+
+        [Route("api/Activities")]
+        public dynamic GetAcitivities()
+        {
+            return new             {
+                _hostingEnvironment.EnvironmentName,
+                Activities = _activities
+            };
+        }
+    }
+}
